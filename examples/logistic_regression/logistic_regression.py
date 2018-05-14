5a5c61b43e583c408b3a2ed1a819db9f3f2c9739
高文文 <gaowenwencn@163.com>
2018-05-14 15:28:14 +0800

    logistic regression: 线性模型包含常数项，在构造数据时，对应列恒为1

--- examples/logistic_regression/logistic_regression.py
+++ examples/logistic_regression/logistic_regression.py
@@ -8,8 +8,8 @@
     Cons: Prone to underfitting, may have low accuracy
     Works with: Numeric values, nominal values
 """
+import matplotlib.pyplot as plt
 import numpy as np
-
 
 """
     modelling a Equation  using  features and it will predict a class for a given input 
@@ -23,9 +23,16 @@ import numpy as np
 """
 
 
-def load_data(file_name):
+def load_data():
+    file_name = r'E:\PycharmProjects\ml_impl\examples\logistic_regression\data\testSet.txt'
     data = np.loadtxt(file_name, delimiter='\t')
     X = data[:, 0:-1]
+
+    # 常数项恒为1 y = w0 + w1x1 +w2x2
+    m = X.shape[0]
+    ones = np.ones((m, 1))
+    X = np.column_stack((ones, X))
+
     y = data[:, -1:]
     return X, y
 
@@ -34,7 +41,7 @@ def sigmoid(z):
     return 1.0 / (1 + np.exp(-z))
 
 
-def gradient_decent(X, y, alpha=0.001, max_niter=3000, epsilon=1e-6):
+def gradient_decent(X, y, alpha=0.001, max_niter=3000, epsilon=1e-10):
     m, n = X.shape
     w = np.ones((n, 1))
 
@@ -51,15 +58,31 @@ def gradient_decent(X, y, alpha=0.001, max_niter=3000, epsilon=1e-6):
     return w
 
 
+def plot_decision_boundry(weight):
+    X, y = load_data()
 
+    ravel_y = y.ravel()
 
+    X0 = X[ravel_y == 0]
+    X1 = X[ravel_y == 1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(X0[:, 0], X0[:, 1], s=30, c='red', marker='s')
+    ax.scatter(X1[:, 0], X1[:, 1], s=30, c='green')
+
+    lineX = np.linspace(-3.0, 3.0, 60)
+    lineY = weight[1] + weight[0] * lineX
+    ax.plot(lineX, lineY)
+    plt.xlabel('X0')
+    plt.ylabel('X1')
+    plt.show()
 
 
 if __name__ == '__main__':
     print('Logistic Regression')
-
-    file_name = r'E:\PycharmProjects\ml_impl\examples\logistic_regression\data\testSet.txt'
-
-    X, y = load_data(file_name)
+    X, y = load_data()
 
     weight = gradient_decent(X, y)
+
+    plot_decision_boundry(weight)
