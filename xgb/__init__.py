e87843957d531dc6b3e9e9eddd3aa59e3a3824fd
高文文 <gaowenwencn@163.com>
2019-12-20 17:34:17 +0800

    xgboost

---
+++ xgb/__init__.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+"""
+ Author: govind
+ Date: 2018/7/13
+ Description: 
+"""
---
+++ xgb/xgboost_learn1.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+"""
+ Author: govind
+ Date: 2018/7/13
+ Description: 
+"""
+
+import xgboost.
